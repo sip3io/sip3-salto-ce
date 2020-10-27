f4f7c5a1b23b92eedc3e1e2f1a13e2f8f71fe647
Aleksandr <leo@sip3.io>
2020-10-27 22:07:42 +0700

    Added: Metrics calculation for invalid SIP messages Added: Test cases

--- src/test/kotlin/io/sip3/salto/ce/sip/SipMessageHandlerTest.kt
+++ src/test/kotlin/io/sip3/salto/ce/sip/SipMessageHandlerTest.kt
@@ -17,6 +17,10 @@
 package io.sip3.salto.ce.sip
 
 import gov.nist.javax.sip.message.SIPMessage
+import io.micrometer.core.instrument.Metrics
+import io.micrometer.core.instrument.MockClock
+import io.micrometer.core.instrument.simple.SimpleConfig
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import io.sip3.commons.vertx.test.VertxTest
 import io.sip3.commons.vertx.util.localRequest
 import io.sip3.salto.ce.RoutesCE
@@ -77,6 +81,56 @@ class SipMessageHandlerTest : VertxTest() {
 
                     """.trimIndent().toByteArray()
         }
+
+        // SIP Message without `Call-ID` Header
+        val PACKET_2 = Packet().apply {
+            timestamp = Timestamp(System.currentTimeMillis())
+            srcAddr = Address().apply {
+                addr = "127.0.0.1"
+                port = 5060
+            }
+            dstAddr = Address().apply {
+                addr = "127.0.0.1"
+                port = 5060
+            }
+            payload = """
+                        SIP/2.0 100 Trying
+                        Via: SIP/2.0/UDP 176.9.119.117:5063;branch=z9hG4bK-2196628568-3926998818-1774583950-1258246515;received=176.9.119.117;rport=5063
+                        From: <sip:123@176.9.119.117:5063;user=phone>;tag=3997885528-3926998818-1774583950-1258246515
+                        To: <sip:321@116.203.55.139;user=phone>
+                        CSeq: 1 INVITE
+                        Server: Asterisk PBX 13.29.1
+                        Allow: INVITE,ACK,CANCEL,OPTIONS,BYE,REFER,SUBSCRIBE,NOTIFY,INFO,PUBLISH,MESSAGE
+                        Supported: replaces,timer
+                        Content-Length: 0
+                    """.trimIndent().toByteArray()
+        }
+
+        // SIP Message without `Call-ID` Header
+        val PACKET_3 = Packet().apply {
+            timestamp = Timestamp(System.currentTimeMillis())
+            srcAddr = Address().apply {
+                addr = "127.0.0.1"
+                port = 5060
+            }
+            dstAddr = Address().apply {
+                addr = "127.0.0.1"
+                port = 5060
+            }
+            payload = """
+                        OPTIONS sip:1010@192.168.10.123:61540;rinstance=afd8f6ae9bf996b2 SIP/2.0
+                        Via: SIP/2.0/UDP 192.168.10.5:5060;rport;branch=z9hG4bKPj5464cb3a-5b18-4e5d-97b1-1cbbd70cb879
+                        From: <sip:1010@192.168.10.5>;tag=883bc998-e361-4d38-ab78-8a543da457f1
+                        To: <sip:1010@192.168.10.123;rinstance=afd8f6ae9bf996b2>
+                        Contact: <sip:1010@192.168.10.5:5060>
+                        Call-ID: 7061e3c4-d807-4cce-859f-2bef92d9f4e0
+                        CSeq: 4358 OPTIONS
+                        Max-Forwards: 70
+                        User-Agent: FPBX-14.0.13.23(13.29.2)
+                        Content-Length:  0
+
+                    """.trimIndent().toByteArray()
+        }
     }
 
     @Test
@@ -90,7 +144,7 @@ class SipMessageHandlerTest : VertxTest() {
                 },
                 assert = {
                     vertx.eventBus().consumer<Pair<String, JsonObject>>(RoutesCE.mongo_bulk_writer) { event ->
-                        var (collection, operation) = event.body()
+                        val (collection, operation) = event.body()
 
                         val document = operation.getJsonObject("document")
 
@@ -120,7 +174,7 @@ class SipMessageHandlerTest : VertxTest() {
                 },
                 assert = {
                     vertx.eventBus().consumer<Pair<Packet, SIPMessage>>(RoutesCE.sip + "_transaction_0") { event ->
-                        var (packet, _) = event.body()
+                        val (packet, _) = event.body()
 
                         context.verify {
                             assertEquals(PACKET_1, packet)
@@ -160,6 +214,65 @@ class SipMessageHandlerTest : VertxTest() {
     }
 
     @Test
+    fun `Handle packet with invalid SIP message`() {
+        val registry = SimpleMeterRegistry(SimpleConfig.DEFAULT, MockClock())
+        Metrics.addRegistry(registry)
+
+        runTest(
+                deploy = {
+                    vertx.deployTestVerticle(SipMessageHandler::class)
+                },
+                execute = {
+                    vertx.eventBus().localRequest<Any>(RoutesCE.sip, PACKET_2)
+                },
+                assert = {
+                    vertx.setPeriodic(200L) {
+                        registry.find(RoutesCE.sip + "_invalid_messages").counter()?.let { counter ->
+                            context.verify {
+                                assertEquals(1.0, counter.count())
+                                val tags = counter.id.tags
+                                assertTrue(tags.isNotEmpty())
+                                assertTrue(tags.any { it.value == "INVITE" })
+                            }
+                            context.completeNow()
+                        }
+                    }
+                }
+        )
+    }
+
+    @Test
+    fun `Handle packet with SIP message and exclude it by CSeq method`() {
+        runTest(
+                deploy = {
+                    vertx.deployTestVerticle(SipMessageHandler::class, JsonObject().apply {
+                        put("sip", JsonObject().apply {
+                            put("message", JsonObject().apply {
+                                put("exclusions", listOf("OPTIONS"))
+                            })
+                        })
+                    })
+                },
+                execute = {
+                    vertx.eventBus().localRequest<Any>(RoutesCE.sip, PACKET_3)
+                    vertx.setPeriodic(1000L) {
+                        vertx.eventBus().localRequest<Any>(RoutesCE.sip, PACKET_1)
+                    }
+                },
+                assert = {
+                    vertx.eventBus().consumer<Pair<Packet, SIPMessage>>(RoutesCE.sip + "_transaction_0") { event ->
+                        val (packet, _) = event.body()
+
+                        context.verify {
+                            assertEquals(PACKET_1, packet)
+                        }
+                        context.completeNow()
+                    }
+                }
+        )
+    }
+
+    @Test
     fun `Apply Groovy UDF to packet with SIP message`() {
         runTest(
                 deploy = {
@@ -184,7 +297,7 @@ class SipMessageHandlerTest : VertxTest() {
                 },
                 assert = {
                     vertx.eventBus().consumer<Pair<Packet, SIPMessage>>(RoutesCE.sip + "_transaction_0") { event ->
-                        var (packet, _) = event.body()
+                        val (packet, _) = event.body()
 
                         val attributes = packet.attributes
                         if (attributes.isNotEmpty()) {
@@ -223,7 +336,7 @@ class SipMessageHandlerTest : VertxTest() {
                 },
                 assert = {
                     vertx.eventBus().consumer<Pair<Packet, SIPMessage>>(RoutesCE.sip + "_transaction_0") { event ->
-                        var (packet, _) = event.body()
+                        val (packet, _) = event.body()
 
                         val attributes = packet.attributes
                         if (attributes.isNotEmpty()) {
